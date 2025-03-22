95295abed857d52f6937a013dad3038599e7cc72
Omarmoatz <omar.raouf.002@gmail.com>
2025-03-22 14:12:54 +0200

    add endpoint for downloading an image

--- src/main/java/com/amazon/ecommerce/controllers/ImageController.java
+++ src/main/java/com/amazon/ecommerce/controllers/ImageController.java
@@ -1,9 +1,16 @@
 package com.amazon.ecommerce.controllers;
 
+import java.sql.SQLException;
 import java.util.List;
 
+import org.springframework.http.HttpHeaders;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.ByteArrayResource;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -32,6 +39,17 @@ public class ImageController {
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
                     .body(new ApiResponse("failed to upload the images", files));
         }
+    }
+
+    @GetMapping("/download")
+    public ResponseEntity<Resource> downloadImage(@PathVariable Long imageId) throws SQLException {
+        var img = imageService.getImageById(imageId);
+        var resource = new ByteArrayResource(img.getImage().getBytes(1, (int) img.getImage().length()));
+        return ResponseEntity.ok()
+                .contentType(MediaType.parseMediaType(img.getFilType()))
+                .header(HttpHeaders.CONTENT_DISPOSITION, "attachement; filename=\"" + img.getFilName() + "\"")
+                .body(resource);
+
     }
 
 }
