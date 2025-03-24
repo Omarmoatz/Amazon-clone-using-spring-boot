c27f79c5be83f1af7ce433ee61cd96435a3e2ba7
Omarmoatz <omar.raouf.002@gmail.com>
2025-03-24 15:46:43 +0200

    add GlobalException

---
+++ src/main/java/com/amazon/ecommerce/exceptions/GlobalException.java
@@ -0,0 +1,26 @@
+package com.amazon.ecommerce.exceptions;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import com.amazon.ecommerce.responses.ApiResponse;
+
+
+
+@RestControllerAdvice
+public class GlobalException {
+
+    @ExceptionHandler(ResourceNotFoundException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public ApiResponse handleResourceNotFound(ResourceNotFoundException e){
+        return new ApiResponse(e.getMessage(), null);
+    }
+
+    @ExceptionHandler(Exception.class)
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    public ApiResponse handleInternalServerError(Exception e){
+        return new ApiResponse(e.getMessage(), null);
+    }
+}
