479fa6377c5f52b7ed5afd0a929b426a6dddb7f7
Omarmoatz <omar.raouf.002@gmail.com>
2025-05-03 17:39:39 +0300

    add spring security & customize it

---
+++ src/main/java/com/amazon/ecommerce/config/SecurityConfig.java
@@ -0,0 +1,42 @@
+package com.amazon.ecommerce.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http.csrf(customizer -> customizer.disable())
+                .authorizeHttpRequests(request -> request.anyRequest().authenticated())
+                .httpBasic(Customizer.withDefaults())
+                .build();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+
+        var user1 = User.withDefaultPasswordEncoder()
+                .username("omar")
+                .password("12345")
+                .roles("ADMIN")
+                .build();
+        
+        var user2 = User.withDefaultPasswordEncoder()
+                .username("ahmed")
+                .password("12345")
+                .roles("USER")
+                .build();
+
+        return new InMemoryUserDetailsManager(user1, user2);
+    }
+}
